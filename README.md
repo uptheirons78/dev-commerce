96b8fcda803e611d478758cd1c1bbb3d4954acf9
Mauro Bono <bono.mauro@gmail.com>
2019-12-20 16:53:02 +0100

    Connected MongoDB with Express server

--- README.md
+++ README.md
@@ -5,3 +5,5 @@ A Full Stack Ecommerce project built with MERN: MongoDB, Express, React and Node
 #### Back End (Step by step)
 
 * Create Express server after `npm i express nodemon dotenv`;
+* Create a Database on MongoDB Atlas (or with a local installation of MongoDB);
+* Install Mongoose and connect with your DB;
--- app.js
+++ app.js
@@ -1,10 +1,26 @@
-const express = require("express");
-const app = express();
-
 require("dotenv").config();
+const express = require("express");
+const mongoose = require("mongoose");
+
+// Application
+const app = express();
 
 // Variables
 const PORT = process.env.PORT || 8000;
+
+// Database Connection
+const connectDB = (async () => {
+  try {
+    await mongoose.connect(
+      process.env.DATABASE,
+      { useNewUrlParser: true, useCreateIndex: true, useUnifiedTopology: true }
+    );
+    console.log("DB Connected ...");
+  } catch (error) {
+    console.error(error.message);
+    process.exit(1);
+  }
+})();
 
 app.get("/", (req, res) => {
   res.send("Home");
